874aa6892e89c47bf663636f0dd71300e1d16562
bonnie <zmffhqj714@naver.com>
2022-05-24 19:15:15 +0900

    20220524

---
+++ src/Algorithm_CodeUp/q1089.java
@@ -0,0 +1,14 @@
+package Algorithm_CodeUp;
+
+import java.util.Scanner;
+
+public class q1089 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int cha = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((cha*n)-(cha-s));
+    }
+}
