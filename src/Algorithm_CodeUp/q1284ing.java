fd4f1e55d2272bc2cc32251dff6bc7b427a52b38
bonnie <zmffhqj714@naver.com>
2022-05-31 20:15:16 +0900

    x_x

--- src/Algorithm_CodeUp/q1284ing.java
+++ src/Algorithm_CodeUp/q1284ing.java
@@ -6,21 +6,66 @@ public class q1284ing {
 
     //! my code
     public static void main(String[] args) {
-        int x, y = 0, i, k = 0;
-double r;
+        int x, y = 0, j, i, k = 0;
+        int r = 0;
         Scanner sc = new Scanner(System.in);
         x = sc.nextInt();
 
-        for (i = 1; i <= 10000000; i++) {
-            for (k = 1; k <= 10000000; k++) {
-               if(i*k == x){
-                   System.out.println(i+" "+k);
-               }
+        if (x == 1 || x == 2) {
+            System.out.println("wrong number");
+        } else {
+            for (i = 2; i <= x; i++) {
+                if (x % i == 0) {
+                    y = i;
+                    break;
+                }
             }
+
+                for (j = y ; j <= x; j++) {
+                    if ((y * j) == x) {
+                        k = j;
+                        System.out.println(y + " " + k);
+                        break;
+                    }
+                }
+                if(i*j!=x){
+                    System.out.println("wrong number");
+                }
+
+
         }
+
+
+
 
     }
 }
+
+
+
+
+//        for (i = 1; i <= x; i++) {
+//            if(x==2){
+//                break;
+//            }
+//            if(x%i==0){
+//                r ++;
+//            }if(r==2){
+//            for (k = 1; k <= x; k++) {
+//                if (i * k == x) {
+//                    System.out.println(i + " " + k);
+//                    break;
+//                }
+//            }}
+//            }
+//        if(r <2){
+//            System.out.println("wrong number");
+//        }
+
+
+
+
+
 
 
 
---
+++ src/Algorithm_CodeUp/q1285.java
@@ -0,0 +1,90 @@
+package Algorithm_CodeUp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class q1285 {
+
+    //! my code
+    public static void main(String[] args) {
+        String []xx ; String mul[],di[],plus[],minus[];
+        String[] x ;
+        int y = 0, j, i;
+        int r = 0;
+        Scanner sc = new Scanner(System.in);
+
+        x = sc.nextLine().split("");
+
+
+//
+//        mul =  new String[x.length()];
+//        di =  new String[xx.length()];
+//        plus =  new String[xx.length()];
+//        minus =  new String[xx.length()];
+//
+//        mul = xx.split("\\*");
+//        //홀수만연산
+//        di = xx.split("/");
+//        plus = xx.split("\\+");
+//        minus = xx.split("-");
+//
+//        System.out.println();
+//
+//
+//        for (i = 1; i <= xx.length()-2; i++) {
+//            if(x[i].equals("=")){
+//                System.out.println(r);
+//                break;
+//            }
+//            switch (x[i]){
+//                case "*":
+//                    r *= Integer.parseInt(x[i+1]);
+//                    break;
+//                case "/":
+//                    r /= Integer.parseInt(x[i+1]);
+//
+//                    break;
+//                case  "+":
+//                    r += Integer.parseInt(x[i+1]);
+//
+//                    break;
+//                case  "-":
+//                    r -= Integer.parseInt(x[i+1]);
+//
+//                    break;
+//                default:
+//
+//            }
+//        }
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
